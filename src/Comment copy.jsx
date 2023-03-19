d5da225903c551efb22f0d957b7bb45a8b8a9e7f
beautifulchoi <ccyjun123@naver.com>
2023-03-19 14:40:58 +0900

    comment page

--- src/Comment copy.jsx
+++ src/Comment copy.jsx
@@ -1,29 +1,53 @@
-import React, { useRef, useState } from 'react';
-
+import React, { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
+import './Comment.css';
 function WriteComment({handleCommentSubmit, nextId}){ //정보 전달은 상위 컴포넌트에서
   const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
   const [comment, setComment] = useState("");
-  
   const handleNicknameChange = (e) => {
     setNickname(e.target.value);
   };
-
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
   const handleCommentChange = (e) => {
     setComment(e.target.value);
   };
   const handleSubmit = () => {
-    handleCommentSubmit(nextId.current, nickname, comment);
+    handleCommentSubmit(nextId.current, nickname, password, comment);
     setNickname("");
     setComment("");
-    nextId.current += 1;
+    nextId.current += 1;//백엔드 서버주소 오면 삭제해야함
+
+    //서버로 댓글 정보 데이터 전송
+    const data = {id: nextId.current, nickname:nickname, password:password, comment:comment};
+    axios.post('/comments', data)
+      .then(response => {
+        // The comment was successfully submitted to the server
+        // Do something, e.g. clear the input fields or show a success message
+        console.log(response);
+        setNickname("");
+        setComment("");
+        nextId.current += 1;
+      })
+      .catch(error => {
+        // There was an error submitting the comment to the server
+        // Do something, e.g. show an error message
+        console.log(error);
+        alert(error);
+      });
   };
   return(
-    <div>
-    <input type="text" placeholder="닉네임" value={nickname} onChange={handleNicknameChange} /> 
-    <br />
-    <textarea placeholder="댓글" value={comment} onChange={handleCommentChange} />
-    <br />
-    <button onClick={handleSubmit}>글쓰기</button>
+    <div className="write-comment">
+      <form>
+        <div className="input-group">
+          <input type="text" placeholder="닉네임" value={nickname} onChange={handleNicknameChange} />
+          <input type="text" placeholder="비밀번호" value={password} onChange={handlePasswordChange} /> 
+        </div>
+      </form> 
+      <textarea placeholder="댓글" value={comment} onChange={handleCommentChange} />
+      <button type="submit" onClick={handleSubmit}>글쓰기</button>
     </div>
   );
 }
@@ -64,10 +88,26 @@ function CommentBoard({users, onEdit, onRemove}){
 
 function Comment(){ 
   const [users, setUsers] = useState([]);
+  //리렌더링시에 댓글리스트 가져오기
+  useEffect(() => {
+    //함수 async 
+    //
+    /**
+     *  await axios.get("/api/comments").then((response) => {
+      setUsers(response.data);
+    });
+     * 
+     */
+    
+    //함수(); 실행
+
+
+
+  }, []);//배열에 인자 넣어야함
   
-  const handleCommentSubmit = (id,nickname, comment) => {
+  const handleCommentSubmit = (id,nickname, password,comment) => {
     if ((nickname!=="")&&(comment!=="")){
-    const newUser = {id:id, nickname: nickname, comment: comment};
+    const newUser = {id:id, nickname: nickname, password:password, comment: comment};
     setUsers([...users, newUser]);
     }
     else if((nickname==="")){
@@ -76,6 +116,7 @@ function Comment(){
     else if((comment==="")){
       alert('코멘트를 작성해주세요');
     }
+    
   };
   
   const onRemove = id => {
